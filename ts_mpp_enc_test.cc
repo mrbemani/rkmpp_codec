613c9dd06c7456f46d279bdb8c4f880e5ed2b648
Mr.Bemani <kawaii_dav@hotmail.com>
2023-10-29 21:47:46 +0800

    update cmake and test

--- ts_mpp_enc_test.cc
+++ ts_mpp_enc_test.cc
@@ -1,98 +1,68 @@
+/******
+ * 
+ *  
+ */
+
+#include <opencv2/opencv.hpp>
+#include "mpp/mpp/inc/mpp.h"
 
 
-#include <opencv4/opencv2/opencv.hpp>
-#include <rockchip/mpp_rc_api.h>
-#include <rockchip/mpp_buffer.h>
-#include <rockchip/mpp_frame.h>
-#include <rockchip/mpp_packet.h>
 
-// ... Your setup code ...
-
-#define TS_MAX_PATH_LEN 256
-
-
-MppFrame CvFrameToMppFrame(const cv::Mat& cvFrame)
-{
-    // Check if the color conversion is needed (e.g., from BGR to YUV)
-    cv::Mat yuvFrame;
-    cv::cvtColor(cvFrame, yuvFrame, cv::COLOR_BGR2YUV_I420);
-
-    // Allocate a buffer for the MPP frame
-    MppBuffer buffer;
-    mpp_buffer_get(NULL, &buffer, yuvFrame.total() * yuvFrame.elemSize());
-
-    // Copy the data from the OpenCV frame to the MPP buffer
-    memcpy(mpp_buffer_get_ptr(buffer), yuvFrame.data, mpp_buffer_get_size(buffer));
-
-    // Create and initialize an MPP frame
-    MppFrame mpp_frame;
-    mpp_frame_init(&mpp_frame);
-    mpp_frame_set_width(mpp_frame, cvFrame.cols);
-    mpp_frame_set_height(mpp_frame, cvFrame.rows);
-    mpp_frame_set_buffer(mpp_frame, buffer);
-
-    return mpp_frame;
-}
-
-
-void ts_encode_frame(MppApi *api, MppCtx ctx, MppEncCfg cfg, const cv::Mat& cvFrame, unsigned char output_filename[TS_MAX_PATH_LEN]) 
-{
-    // Convert the OpenCV frame to an MPP frame
-    if (frame.empty()) {
-        break;  // Break the loop if we've read all frames
+int main() {
+    // Open the RTSP feed using OpenCV
+    cv::VideoCapture cap("rtsp://your_rtsp_feed_url");
+    if (!cap.isOpened()) {
+        std::cerr << "Error: Unable to open the RTSP feed!" << std::endl;
+        return -1;
     }
 
-    // Convert the OpenCV frame to a format that MPP can understand, if necessary
-    // This may involve converting the color space, reformatting the data, etc.
-    // Assume CvFrameToMppFrame is a function you've defined to handle this conversion
-    MppFrame mpp_frame = CvFrameToMppFrame(frame);
-
-    // Set up the MPP encoding context, if not already set up
-    // This would typically be done outside the loop, before entering it
-
-    // Encode the frame
-    MppPacket packet;
-    mpp_encode(api, mpp_frame, &packet);
-
-    // Write the encoded data to your output file
-    // Assume writePacketToFile is a function you've defined to handle this
-    writePacketToFile(packet, "output_video.mp4");
-
-    // Free any resources associated with this frame
-    mpp_frame_deinit(&mpp_frame);
-    mpp_packet_deinit(&packet);
-}
-
-
-
-void destroyContext(MppApi *api, MppCtx ctx) {
-    // Destroy the MPP context
-    mpp_destroy(ctx);
-    mpp_destroy(api);
-}
-
-
-
-int main()
-{
-    MppApi *api = mpp_create();
+    // Initialize Rockchip MPP
     MppCtx ctx;
-    mpp_create(&ctx, &api);
+    MppApi *mpi;
+    mpp_create(&ctx, &mpi);
 
+    // Set encoding parameters (assuming an API similar to FFmpeg)
+    // This is hypothetical and may not match the exact MPP API
     MppEncCfg cfg;
     mpp_enc_cfg_init(&cfg);
-    // Configure the encoder for H264, 1920x1080, 25fps
     mpp_enc_cfg_set_s32(cfg, "width", 1920);
     mpp_enc_cfg_set_s32(cfg, "height", 1080);
-    mpp_enc_cfg_set_s32(cfg, "fps", 25);
+    mpp_enc_cfg_set_s32(cfg, "rc_mode", MPP_ENC_RC_MODE_VBR);
+    mpp_enc_cfg_set_s32(cfg, "format", MPP_FMT_YUV420SP);
+    mpp_enc_cfg_set_s32(cfg, "codec", MPP_VIDEO_CodingHEVC);
+    mpi->control(ctx, MPP_ENC_SET_CFG, cfg);
 
-    mpp_enc_cfg_set_s32(cfg, "rc_mode", MPP_ENC_RC_MODE_CBR);
-    mpp_enc_cfg_set_s32(cfg, "bps", 5000000);  // Set bitrate, e.g., 5Mbps
+    cv::Mat frame;
+    MppBuffer input_buf, output_buf;
 
+    while (true) {
+        cap >> frame;
+        if (frame.empty()) break;
 
-    
-    // Initialize the MPP encoder
-    mpp_enc_init(ctx, cfg);
+        // Assume MPP requires a specific buffer format
+        mpp_buffer_get(NULL, &input_buf, frame.total() * frame.elemSize());
+        memcpy(mpp_buffer_get_ptr(input_buf), frame.data, frame.total() * frame.elemSize());
 
-    
+        // Encode the frame using MPP
+        MppPacket packet = NULL;
+        mpi->encode_put_frame(ctx, input_buf);
+        mpi->encode_get_packet(ctx, &packet);
+
+        if (packet) {
+            // The encoded data should now be available in `packet`
+            // Write the encoded data to a file, transmit over network, etc.
+            // ...
+
+            // Release the packet
+            mpp_packet_deinit(&packet);
+        }
+
+        // Release the input buffer
+        mpp_buffer_put(input_buf);
+    }
+
+    // Finalize MPP and release resources
+    mpp_destroy(ctx);
+
+    return 0;
 }
